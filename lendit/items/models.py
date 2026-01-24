24cde204d31bf3a5679511c20f277333d19810c5
boris-isac <borisisac@gmail.com>
2026-01-24 03:43:52 +0000

    [ADD] item models and image item model

--- lendit/items/models.py
+++ lendit/items/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 
@@ -8,3 +10,34 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class City(models.TextChoices):
+    LISBON = "LIS", "Lisbon"
+    PORTO = "OPO", "Porto"
+    VILA_NOVA_DE_GAIA = "VNG", "Vila Nova de Gaia"
+    SINTRA = "SIN", "Sintra"
+    CASCAIS = "CAS", "Cascais"
+    AMADORA = "AMA", "Amadora"
+    COIMBRA = "COI", "Coimbra"
+    BRAGA = "BRA", "Braga"
+    SETUBAL = "SET", "Setúbal"
+    AVEIRO = "AVE", "Aveiro"
+    FARO = "FAO", "Faro"
+    GUIMARAES = "GUI", "Guimarães"
+    VISEU = "VIS", "Viseu"
+    LEIRIA = "LEI", "Leiria"
+    EVORA = "EVO", "Évora"
+    PORTIMAO = "PRM", "Portimão"
+    FUNCHAL = "FNC", "Funchal"
+
+class Item(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='items')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='items')
+    city = models.CharField(max_length=50, choices=City.choices, default=City.LISBON.value)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='itemImages')
