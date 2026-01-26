d8a8da42be1729568c1daebb02176b79876403a5
boris-isac <borisisac@gmail.com>
2026-01-26 23:01:54 +0000

    [ADD] serializers and redefined create method

---
+++ lendit/items/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from users.serializers  import ShortCustomUserSerializer
+
+from .models import ItemImage, Item
+
+
+class ItemImagesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = ItemImage
+        fields = '__all__'
+
+class ItemSerializer(serializers.ModelSerializer):
+    images = ItemImagesSerializers(many=True)
+    owner_short_data = ShortCustomUserSerializer(read_only=True)
+    city_display_name = serializers.CharField(source='get_city_display', read_only=True)
+    class Meta:
+        model = Item
+        fields = ['title', 'description','owner_short_data','images', 'category','price','city','city_display_name']
+
+
+    def create(self, validated_data):
+        validated_data['owner'] = self.context['request'].user
+        images = validated_data.pop('images')
+        item = Item.objects.create(**validated_data)
+
+        lst = [ItemImage(item=item, **image) for image in images]
+
+        ItemImage.objects.bulk_create(lst)
+
+        return item
+
--- lendit/users/serializers.py
+++ lendit/users/serializers.py
@@ -18,4 +18,9 @@ class CustomUserSerializers(serializers.ModelSerializer):
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
 
+class ShortCustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('email','first_name','phone')
+
 
